321407faa8bc32f276ad636fc59822e6210613c1
Nikita Novokshonov <Nikita.Novokhnov@gmail.com>
2020-11-14 19:04:41 +0500

    задачи 5-го модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let slideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  let allSlides = document.querySelectorAll('.carousel__slide').length - 1;
+
+  let elementCount = 0;
+  leftArrow.style.display = "none";
+
+  rightArrow.addEventListener('click', function(event) {
+    elementCount ++;
+    if(elementCount == 1){
+      leftArrow.style.display = "";
+    }
+  
+    if(elementCount == allSlides){
+      rightArrow.style.display = "none";
+    }
+    carouselInner.style.transform = 'translateX(-'+slideWidth*elementCount+'px)';
+  });
+
+  leftArrow.addEventListener('click', function(event) {
+    elementCount --;
+    if(elementCount == 0){
+      leftArrow.style.display = "none";
+    }
+  
+    if(elementCount == allSlides - 1){
+      rightArrow.style.display = "";
+    }
+    carouselInner.style.transform = 'translateX(-'+slideWidth*elementCount+'px)';
+  });
 }
