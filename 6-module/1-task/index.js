f1a5c900ada690623c30b1f94d9e8b777daca7c5
Nikita Novokshonov <Nikita.Novokhnov@gmail.com>
2020-11-15 20:44:06 +0500

    задача 6-го модуля\1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,39 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem =  document.createElement('table');
+    let thead = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+    `;
+
+    let rowsHandler = '';
+    rows.forEach(function(item,index,array){
+      rowsHandler += `
+        <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</button></td>
+        </tr>
+        `;
+    },this);
+    this.elem.innerHTML += thead;
+    this.elem.innerHTML += rowsHandler;
+    this.elem.addEventListener('click', this.deleteRow);
   }
+
+  deleteRow(e){
+    if(e.target.tagName != "BUTTON") return;
+    e.target.parentElement.parentElement.remove();
+  }
+
 }
